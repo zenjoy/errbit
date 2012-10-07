b5c9467755e9c1f69ca1e79e56f9c0ac2ac61a10
Peter Dedene <peter@dedene.com>
2012-10-07 17:41:07 +0200

    Added some estetic improvements to the error notifications

--- app/models/notification_services/hipchat_service.rb
+++ app/models/notification_services/hipchat_service.rb
@@ -19,10 +19,18 @@ if defined? HipChat
 
     def create_notification(problem)
       url = app_problem_url problem.app, problem
+      
+      last_error = problem.errs.last
+      last_notice = last_error.notices.last if last_error
+      error_url = last_notice.request['url'] if last_notice && last_notice.request
+
       message = <<-MSG.strip_heredoc
-        [#{ERB::Util.html_escape problem.app.name}]#{ERB::Util.html_escape notification_description(problem)}<br>
-        <a href="#{url}">#{url}</a>
+        <b>#{ERB::Util.html_escape problem.app.name}</b>
+        (<a href='#{url}'>view error</a>)
+        <br>&nbsp;-&nbsp;<b>#{problem.message.to_s.truncate(100)} [#{ problem.environment }] [#{ problem.where }]</b>
       MSG
+
+      message << "triggered at <a href='#{CGI::escapeHTML(error_url)}'>#{CGI::escapeHTML(error_url)}</a>" if error_url
 
       client = HipChat::Client.new(api_token)
       client[room_id].send('Errbit', message, :color => 'red')
